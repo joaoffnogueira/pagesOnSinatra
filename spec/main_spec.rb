b1da23641083fc725a53a69ff581a4a6d9bb0486
joao.nogueira <joao.nogueira@codeminer42.com>
2022-01-27 17:35:39 -0300

    add rack-test and test for route to page1

---
+++ spec/main_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require_relative '../main'
+require 'rspec'
+require 'rack/test'
+
+RSpec.configure do |conf|
+  conf.include Rack::Test::Methods
+end
+
+RSpec.describe 'Main' do
+  describe 'GET /' do
+    def app
+      Sinatra::Application
+    end
+
+    it 'get the about page' do
+      get '/'
+      expect(last_response).to be_ok
+    end
+  end
+end
