397cc46e0748df7bba398ff0bba26845815fe925
joao.nogueira <joao.nogueira@codeminer42.com>
2022-01-27 17:32:55 -0300

    create page1

--- main.rb
+++ main.rb
@@ -1,22 +1,29 @@
 # frozen_string_literal: true
 
 require 'sinatra'
+require 'json'
 
 get '/' do
   @title = 'About'
-  @msg = 'Hello world!'
-  erb :layout
+  @main_content = ['Hi there! I am João, a former biology teacher making a career transition to technology as a
+     developer.', 'My current study and work are focused on Ruby, but I have some experience with Java, Kotlin and
+     Flutter for mobile development.', 'I have academic experience in technical research and extension projects, which
+      helped me to develop communicative skills, teamwork, collaboration, experience with event organization and project
+       management, as well as conduct research to solve problems with continuous learning.', 'I also have teaching
+        experience with elementary, high school and university students. I am always willing to learn new technologies
+         from both the front-end and back-end. My current knowledge includes a bit of web, and C.']
+  erb :index
 end
 
 get '/prs' do
   @title = 'PRs'
-  @msg = 'Hello world!'
-  #https://api.github.com/repos/joaoffnogueira/filesWithRuby/pulls?state=all
+  @main_content = 'Hello world!'
+  # https://api.github.com/repos/joaoffnogueira/filesWithRuby/pulls?state=all
   erb :layout
 end
 
 get '/resume' do
   @title = 'Resume'
-  @msg = 'Hello world!'
+  @main_content = 'Hello world!'
   erb :layout
 end
